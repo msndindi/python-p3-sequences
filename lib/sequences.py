1a6ce8a6c05a443cdfb831561fbb3f381a44c456
Mutindi Hope <msndindi@outlook.com>
2023-05-20 22:12:29 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,13 @@
 
 def print_fibonacci(length):
     pass
+    my_list = []
+
+    if length == 0:
+        print(my_list)
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    elif length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
